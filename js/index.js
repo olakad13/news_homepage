79cbc1bee419dc55fbdf3b84f886bc53c2a40ee2
olakad13 <olakad13@gmail.com>
2023-03-23 12:37:34 +0100

    worked on the js

--- js/index.js
+++ js/index.js
@@ -33,13 +33,13 @@ window.addEventListener("resize", () => {
 
         navBar.style.transform = "translateX(0)";
 
-    } else if (screen.width , 900) {
+    } else if (screen.width < 900) {
 
         navBar.style.transform = "translateX(100vw)";
 
     } else if (window.innerWidth >= 900) {
         navBar.style.transform = "translateX(0)";
-    } else if (window.innerWidth , 900) {
+    } else if (window.innerWidth < 900) {
         navBar.style.transform = "translateX(100vw)";
     }
 });
